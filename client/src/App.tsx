69e656b09021d6e6573574bf119f7c2925c59a54
BARmonse <brunorius3@gmail.com>
2023-04-28 22:17:14 -0300

    Adding socket.io

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+
 function App() {
+
+  useEffect(() => {
+    const socket = io("localhost:/5000/", {
+      transports: ["websocket"]
+    });
+
+    socket.emit("test")
+
+    return () => {
+      console.log("DISCONNECTING")
+      socket.disconnect()
+    }
+  }, [])
+
   return (
    <></>
   )
