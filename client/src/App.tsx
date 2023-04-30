a29be3343fe70fcbbe29ae49032f74d4b38160db
BARmonse <brunorius3@gmail.com>
2023-04-30 20:24:08 -0300

    Adding automatic formating on save

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,22 @@
-import { useEffect } from "react";
-import { io } from "socket.io-client";
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 function App() {
-
   useEffect(() => {
-    const socket = io("http://localhost:5000/", {
-      transports: ["websocket"]
+    const socket = io('http://localhost:5000/', {
+      transports: ['websocket'],
     });
 
-    socket.emit("test")
+    socket.emit('test');
 
-    socket.on("test", () => console.log("HOLAA"))
+    socket.on('test', () => console.log('HOLAA'));
 
     return () => {
-      socket.disconnect()
-    }
-  }, [])
+      socket.disconnect();
+    };
+  }, []);
 
-  return (
-   <></>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
