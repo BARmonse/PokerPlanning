e32f56b158e5785c313835822c2bea4007939791
BARmonse <brunorius3@gmail.com>
2023-05-01 00:57:14 -0300

    Adding a generic header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import { Dashboard } from './components/Dashboard';
 import { Colors } from './enums/Colors';
 import './index.css';
 import { makeStyles } from '@mui/styles';
+import { Header } from './components/Header';
 
 const App = () => {
   const classes = useStyles();
   return (
     <div className={classes.appContainer}>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Dashboard />} />
         </Routes>
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { makeStyles } from '@mui/styles';
+import { Colors } from '../enums/Colors';
+import { Typography } from '@mui/material';
+
+export const Header = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.headerContainer}>
+      <Typography className={classes.headerTitle}>POKER PLANNING</Typography>
+    </div>
+  );
+};
+
+const useStyles = makeStyles({
+  headerContainer: {
+    minHeight: '10vh',
+    backgroundColor: Colors.NICE_PURPLE,
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    display: 'flex',
+    alignSelf: 'center',
+    color: Colors.WHITE,
+  },
+});
