64292eae08a6f6c15e1ac275526a24895d2f6bbd
BARmonse <brunorius3@gmail.com>
2023-05-04 20:54:41 -0300

    Formatting

--- server/main.py
+++ server/main.py
@@ -3,15 +3,17 @@ from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'holasoyyo'
-CORS(app, resources = {r"/*":{"origins":"*"}})
+app.config["SECRET_KEY"] = "holasoyyo"
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-socket = SocketIO(app, cors_allowed_origins = "*")
+socket = SocketIO(app, cors_allowed_origins="*")
+
 
 @socket.on("test")
 def test():
     print("This is working")
-    emit("test", {"data":f"id: {request.sid} is testing"})
+    emit("test", {"data": f"id: {request.sid} is testing"})
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     socket.run(app, debug=True, port=5000)
