5c4c23e01bb332b9e7ecece98823b2adb6efae7c
BARmonse <brunorius3@gmail.com>
2023-05-06 13:26:04 -0300

    Go installation

--- server/runServer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-reset='\033[0m';
-cyan='\033[0;36m';
-
-echo "\n${cyan}Installing dependencies${reset}\n"
-pip install -r requirements.txt;
-echo "\n${cyan}Running server${reset}\n"
-python3 main.py;
