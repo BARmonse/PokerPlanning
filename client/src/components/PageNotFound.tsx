2b853dc0412af2b785b3db4ac176ce915eef843f
BARmonse <brunorius3@gmail.com>
2023-05-13 19:21:19 -0300

    Moving pagenotfound styles no another file

--- client/src/components/PageNotFound.tsx
+++ client/src/components/PageNotFound.tsx
@@ -1,27 +1,15 @@
-import { Container, Typography } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { Colors } from '../enums/Colors';
+import { Box, Typography } from '@mui/material';
+import {
+  notFoundContainerStyle,
+  notFoundTextStyle,
+} from '../styles/PageNotFound';
 
 export const PageNotFound = () => {
-  const classes = useStyles();
   return (
-    <Container className={classes.notFoundContainer}>
-      <Typography className={classes.notFoundText}>
+    <Box sx={notFoundContainerStyle}>
+      <Typography sx={notFoundTextStyle}>
         Nothing here, please go back...
       </Typography>
-    </Container>
+    </Box>
   );
 };
-
-const useStyles = makeStyles({
-  notFoundContainer: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  notFoundText: {
-    color: Colors.RED,
-    fontWeight: 'bolder',
-    fontSize: '12px',
-    textAlign: 'center',
-  },
-});
