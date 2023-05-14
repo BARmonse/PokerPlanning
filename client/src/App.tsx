5b94fdd53cd23be7ed339d2a694cdc6f2cad1edc
BARmonse <brunorius3@gmail.com>
2023-05-14 20:23:47 -0300

    Moving app styles to another folder

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Dashboard } from './components/Dashboard';
-import { Colors } from './enums/Colors';
 import './index.css';
 import { Header } from './components/Header';
 import { PageNotFound } from './components/PageNotFound';
 import { Box } from '@mui/material';
+import { appContainerStyle } from './styles/App';
 
 const App = () => {
   return (
-    <Box sx={{ backgroundColor: Colors.LIGHT_GRAY, minHeight: '100vh' }}>
+    <Box sx={appContainerStyle}>
       <Router>
         <Header />
         <Routes>
---
+++ client/src/styles/App.ts
@@ -0,0 +1,6 @@
+import { Colors } from '../enums/Colors';
+
+export const appContainerStyle = {
+  backgroundColor: Colors.LIGHT_GRAY,
+  minHeight: '100vh',
+};
